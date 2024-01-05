ddad5231b6eabc444de55577117c33e4912401f6
Ishaanlol91 <144274287+Ishaanlol91@users.noreply.github.com>
2024-01-05 14:24:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-var dice = {
-  sides: 6,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
+var elDiceOne       = document.getElementById('dice1');
+var elDiceTwo       = document.getElementById('dice2');
+var elComeOut       = document.getElementById('roll');
+
+elComeOut.onclick   = function () {rollDice();};
+
+function rollDice() {
+
+  var diceOne   = Math.floor((Math.random() * 6) + 1);
+  var diceTwo   = Math.floor((Math.random() * 6) + 1);
+ 
+  console.log(diceOne + ' ' + diceTwo);
+
+  for (var i = 1; i <= 6; i++) {
+    elDiceOne.classList.remove('show-' + i);
+    if (diceOne === i) {
+      elDiceOne.classList.add('show-' + i);
+    }
   }
+
+  for (var k = 1; k <= 6; k++) {
+    elDiceTwo.classList.remove('show-' + k);
+    if (diceTwo === k) {
+      elDiceTwo.classList.add('show-' + k);
+    }
+  } 
+  setTimeout(rollDice(), 1000);
 }
-
-
-
-//Prints dice roll to the page
-
-function printNumber(number) {
-  var placeholder = document.getElementById('placeholder');
-  placeholder.innerHTML = number;
-}
-
-var button = document.getElementById('button');
-
-button.onclick = function() {
-  var result = dice.roll();
-  printNumber(result);
-};
